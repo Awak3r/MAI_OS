653a75eb47d5d63242ec939fe9fc22cd943c2148
Awak3r <bogdanovmihail129@gmail.com>
2025-09-27 12:32:01 +0300

    try 1

---
+++ lab1/lab1_client.c
@@ -0,0 +1,97 @@
+#include <stdint.h>
+#include <stdbool.h>
+
+#include <unistd.h>
+#include <sys/wait.h>
+#include <stdlib.h>
+#include <stdio.h>
+
+static char SERVER_PROGRAM_NAME[] = "lab1_server";
+
+int main(int argc, char **argv) {
+	if (argc == 1) {
+		char msg[1024];
+		uint32_t len = snprintf(msg, sizeof(msg) - 1, "usage: %s filename\n", argv[0]);
+		write(STDERR_FILENO, msg, len);
+		exit(EXIT_SUCCESS);
+	}
+	char progpath[1024];
+	{
+		ssize_t len = readlink("/proc/self/exe", progpath,
+		                       sizeof(progpath) - 1);
+		if (len == -1) {
+			const char msg[] = "error: failed to read full program path\n";
+			write(STDERR_FILENO, msg, sizeof(msg));
+			exit(EXIT_FAILURE);
+		}
+		while (progpath[len] != '/')
+			--len;
+
+		progpath[len] = '\0';
+	}
+	int client_to_server[2]; 
+	if (pipe(client_to_server) == -1) {
+		const char msg[] = "error: failed to create pipe\n";
+		write(STDERR_FILENO, msg, sizeof(msg));
+		exit(EXIT_FAILURE);
+	}
+
+	int server_to_client[2]; 
+	if (pipe(server_to_client) == -1) {
+		const char msg[] = "error: failed to create pipe\n";
+		write(STDERR_FILENO, msg, sizeof(msg));
+		exit(EXIT_FAILURE);
+	}
+
+
+	const pid_t child = fork();
+
+	switch (child) {
+	case -1: { 
+		const char msg[] = "error: failed to spawn new process\n";
+		write(STDERR_FILENO, msg, sizeof(msg));
+		exit(EXIT_FAILURE);
+	} break;
+	case 0: { 
+		close(client_to_server[1]);
+		close(server_to_client[0]);
+		dup2(client_to_server[0], STDIN_FILENO);
+		close(client_to_server[0]);
+		dup2(server_to_client[1], STDOUT_FILENO);
+		close(server_to_client[1]);
+		{
+			char path[1024];
+			snprintf(path, sizeof(path) - 1, "%s/%s", progpath, SERVER_PROGRAM_NAME);
+			char *const args[] = {SERVER_PROGRAM_NAME, argv[1], NULL};
+			int32_t status = execv(path, args);
+			if (status == -1) {
+				const char msg[] = "error: failed to exec into new exectuable image\n";
+				write(STDERR_FILENO, msg, sizeof(msg));
+				exit(EXIT_FAILURE);
+			}
+		}
+	} break;
+
+	default: { 
+		close(client_to_server[0]);
+		close(server_to_client[1]);
+		char buf[4096];
+		ssize_t bytes;
+		while ((bytes = read(STDIN_FILENO, buf, sizeof(buf)))) {
+			if (bytes < 0) {
+				const char msg[] = "error: failed to read from stdin\n";
+				write(STDERR_FILENO, msg, sizeof(msg));
+				exit(EXIT_FAILURE);
+			} else if (buf[0] == '\n') {
+				break;
+			}
+			write(client_to_server[1], buf, bytes);
+		}
+
+		close(client_to_server[1]);
+		close(server_to_client[0]);
+
+		wait(NULL);
+	} break;
+	}
+}
---
+++ lab1/lab1_server.c
@@ -0,0 +1,121 @@
+#include <stdint.h>
+#include <stdbool.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <fcntl.h>
+#include <stdio.h>
+
+float  buf_to_numbers(char buf[]) {
+    float n = 10, num2 = 0, result;
+    int num_path, i = 0, flag = 0, min_flag = 0, first_flag = 0;
+    while (true){
+        if (buf[i] != '1' && buf[i] != '2' && buf[i] != '3' && buf[i] != '4' && buf[i] != '5' \
+            && buf[i] != '6' && buf[i] != '7' && buf[i] != '8'&& buf[i] != '9' && buf[i] != '0' && buf[i] != '.' && buf[i] != ' ' \
+            && buf[i] != '-' && buf[i] !='\n'){
+                const char msg[] = "error: invalid data\n";
+                write(STDOUT_FILENO, msg, sizeof(msg));
+                exit(EXIT_FAILURE);
+        }
+        if (buf[i] == ' '){
+            n = 10;
+            flag = 0;
+            if (num2 == 0 && first_flag == 1){
+                const char msg[] = "error: can`t devide by zero";
+                write(STDOUT_FILENO, msg, sizeof(msg));
+            }
+            if (min_flag == 1){
+                num2 *= -1;
+            }
+            min_flag = 0;
+            if (first_flag == 0){
+                result = num2;
+                first_flag = 1;
+            }
+            else if (num2 != 0 && first_flag == 1){
+                result /= num2;
+            }
+            else if (num2 == 0 && first_flag == 1){
+                const char msg[] = "error: can`t devide by zero";
+                write(STDOUT_FILENO, msg, sizeof(msg));
+                exit(EXIT_FAILURE);
+            }
+            num2 = 0;
+        }
+        else if (buf[i] == '\n'){
+            n = 10;
+            flag = 0;
+            if (min_flag == 1){
+                num2 *= -1;
+            }
+            min_flag = 0;
+            if (first_flag == 0){
+                result = num2;
+                first_flag = 1;
+            }
+            else if (num2 != 0 && first_flag == 1){
+                result /= num2;
+            }
+            else if (num2 == 0 && first_flag == 1){
+                const char msg[] = "error: can`t devide by zero";
+                write(STDOUT_FILENO, msg, sizeof(msg));
+                exit(EXIT_FAILURE);
+            }
+            num2 = 0;
+            break;
+        }
+        else if (buf[i] == '.'){
+            flag = 1;
+            n = 0.1;
+        }
+        else if (buf[i] == '-'){
+            min_flag = 1;
+        }
+        else if (flag == 0) {
+            num2 = (buf[i] - '0') + num2 * n;
+        }
+        else if (flag == 1) {
+            num2 += (buf[i] - '0') * n;
+            n/=10;
+        }
+        ++i;
+    }
+    return result;
+}
+
+
+
+int main(int argc, char **argv) {
+	char buf[4096];
+	ssize_t bytes;
+	int32_t file = open(argv[1], O_WRONLY | O_CREAT | O_TRUNC, 0600);
+	if (file == -1) {
+		const char msg[] = "error: failed to open requested file\n";
+	    write(STDERR_FILENO, msg, sizeof(msg));
+		exit(EXIT_FAILURE);
+	}
+
+	while ((bytes = read(STDIN_FILENO, buf, sizeof(buf)))) {
+		if (bytes < 0) {
+			const char msg[] = "error: failed to read from stdin\n";
+			write(STDERR_FILENO, msg, sizeof(msg));
+			exit(EXIT_FAILURE);
+		}
+
+        float result = buf_to_numbers(buf);
+        bytes = sprintf(buf, "%.2f\n", result);
+		{
+			int32_t written = write(file, buf, bytes);
+			if (written != bytes) {
+				const char msg[] = "error: failed to write to file\n";
+				write(STDERR_FILENO, msg, sizeof(msg));
+				exit(EXIT_FAILURE);
+			}       
+		}
+	}
+	if (bytes == 0) {
+		const char term = '\0';
+		write(file, &term, sizeof(term));
+	}
+
+	close(file);
+}
